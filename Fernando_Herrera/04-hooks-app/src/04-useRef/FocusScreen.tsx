25357fe5013afcbee23b97bdbe6063aba0b15b18
ppambriz <ppambriz@gmail.com>
2026-09-09 23:49:01 -0600

    useRef

--- Fernando_Herrera/04-hooks-app/src/04-useRef/FocusScreen.tsx
+++ Fernando_Herrera/04-hooks-app/src/04-useRef/FocusScreen.tsx
@@ -1,18 +1,30 @@
-
-
+import { useRef } from "react";
 
 export const FocusScreen = () => {
+  const inputRef = useRef<HTMLInputElement>(null);
+
+  const handleClick = () => {
+    console.log(inputRef.current?.value);
+    inputRef.current?.select();
+
+  }
+
   return (
     <div className="bg-gradient flex-col gap-4">
-
       <h1 className="text-2xl font-thin text-white">Focus Screen</h1>
 
-      <input type="text" 
+      <input
+        ref={inputRef}
+        type="text"
         className="bg-white text-black px-4 py-2 rounded-md"
+        autoFocus
       />
-      
-      
-      
+
+      <button
+        onClick={handleClick}
+        className="bg-blue-500 text-white px-4 py-2 rounded-md cursor-pointer">
+        Set focus
+      </button>
     </div>
-  )
-}
+  );
+};
